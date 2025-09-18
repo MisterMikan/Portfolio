a43fece4817e6376a647f293a313de91425bfc48
MisterMikan <michaelmozo2003@gmail.com>
2025-09-18 10:34:14 -0700

    initial commit

--- PersonalWebsite.js
+++ PersonalWebsite.js
@@ -1,1 +1,9 @@
-alert ("Done");
+function openNav() {
+    document.getElementById("myNavigations").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("myNavigations").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
