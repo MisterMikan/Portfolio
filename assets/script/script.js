eeb629bfbd531eaf6e524c518ec0ef2b7dc6c62c
sirezo <joebertcerezo@gmail.com>
2025-09-20 08:27:56 +0800

    feat: add navigation open and close functions

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNavigations").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("myNavigations").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
