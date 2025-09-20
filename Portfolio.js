ed4a4351cb8b91713d0e234bcfa520eebf1fd7e1
sirezo <joebertcerezo@gmail.com>
2025-09-20 08:27:02 +0800

    chore: remove unused assets and files from portfolio

--- Portfolio.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("myNavigations").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("myNavigations").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
